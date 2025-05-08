4ce9312ca79cbfdcceec5d3bdd992e85f4853d6d
pranita383 <pranitapadaliya23@gmail.com>
2025-05-08 10:33:16 +0530

    error fixing for ast genraration

--- parser.py
+++ parser.py
@@ -1,12 +1,9 @@
-def read_tokens_from_symbol_table(filename):
-    tokens = []
-    with open(filename, 'r') as file:
-        for line in file:
-            if ':' in line:
-                value, token_type = line.strip().split(':')
-                tokens.append((token_type.strip(), value.strip()))
-    return tokens
+import json
 
+# Read tokens from JSON file
+def read_tokens_from_json(filename):
+    with open(filename, 'r') as file:
+        return json.load(file)
 
 class Parser:
     def __init__(self, tokens):
@@ -19,125 +16,96 @@ class Parser:
         raise Exception(f"Parse error: {message}")
 
     def advance(self):
-        """Move to the next token."""
         self.pos += 1
         if self.pos < len(self.tokens):
             self.current_token = self.tokens[self.pos]
         else:
             self.current_token = None
 
-    
     def consume(self, token_type):
-        """Consume the current token if it matches the given token type."""
         if self.current_token is None:
             self.error(f"Unexpected end of input. Expected token type {token_type}.")
         elif self.current_token[0] == token_type:
-            print(f"Consumed token: {self.current_token}")  # Debug print
+            print(f"Consumed token: {self.current_token}")
             self.advance()
-        else:  
+        else:
             self.error(f"Expected token type {token_type}, but got {self.current_token}")
 
+    def parse(self):
+        return self.parse_program()
+
     def parse_program(self):
-        """Parse the entire program."""
         statements = []
         while self.current_token:
-            print(f"Parsing statement: {self.current_token}")  # Debug print}")
+            print(f"Parsing statement: {self.current_token}")
             statements.append(self.parse_statement())
         return {"type": "Program", "body": statements}
 
     def parse_statement(self):
-        """Parse a single statement."""
-        if self.current_token[0] == "IDENTIFIER":
+        if self.current_token[1] == "print" and self.current_token[0] in ("IDENTIFIER","KEYWORD"):
+            
+            return self.parse_print()
+        elif self.current_token[0]=="IDENTIFIER":
             return self.parse_assignment()
         else:
             self.error("Invalid statement")
 
     def parse_assignment(self):
-        """Parse an assignment statement."""
         var_name = self.current_token[1]
         self.consume("IDENTIFIER")
-        self.consume("OPERATOR")  # Equals sign '='
+        self.consume("OPERATOR")  # '='
         expression = self.parse_expression()
-        self.consume("SEPARATOR")  # Semicolon ';'
+        self.consume("SEPARATOR")  # ';'
         return {"type": "Assignment", "name": var_name, "value": expression}
 
-    def parse_expression(self):
-        return self.parse_term()
-    
-    def parse_term(self):
-        node= self.parse_factor()
-        while self.current_token and self.current_token[0]=="OPERATOR" and self.current_token[1]in('+','-'):
-            op = self.current_token[1]
-            self.consume("OPERATOR")
-            right= self.parse_factor()
-            node ={"type":"BinaryExpression","operator": op, "left": node, "right":right}
-        return node
-    
-    def parse_factor(self):
-        if self.current_token[0]=='NUMBER':
-            return self.parse_number()
-        elif self.current_token[0]=='IDENTIFIER':
-            return self.parse_identifier()
-        else:
-            self.error("invalid factor")
-
-    def parse_primary(self):
-        if self.current_token[0] == "NUMBER":
-            return self.parse_number()
-        elif self.current_token[0] == "IDENTIFIER":
-            return self.parse_identifier()
-        elif self.current_token[0] == "SEPARATOR" and self.current_token[1] == '(':
-            self.consume("SEPARATOR")  # consume '('
-            expr = self.parse_expression()
-            self.consume("SEPARATOR")  # consume ')'
-            return expr
-        else:
-            self.error("Invalid primary expression")
-
-    def parse_number(self):
-        """Parse a number."""
-        num = self.current_token[1]
-        self.consume("NUMBER")
-        return {"type": "Number", "value": num}
-
-    def parse_identifier(self):
-        """Parse an identifier."""
-        var_name = self.current_token[1]
-        self.consume("IDENTIFIER")
-        return {"type": "Identifier", "name": var_name}
-
-    def parse(self):
-        return self.parse_program()
-
-    def parse_statement(self):
-        if self.current_token[0] == "IDENTIFIER":
-            if self.current_token[1] == "print":
-                return self.parse_print()
-            else:
-                return self.parse_assignment()
-        else:
-            self.error("Invalid statement")
-
     def parse_print(self):
-        self.consume("IDENTIFIER")  # 'print'
+        if self.current_token[0] in ("IDENTIFIER","KEYWORD") and self.current_token[1]=="print":
+            self.advance()
+        else:
+            self.error(f"exception print keyword but got {self.current_token}")
         self.consume("SEPARATOR")   # '('
         expr = self.parse_expression()
         self.consume("SEPARATOR")   # ')'
         self.consume("SEPARATOR")   # ';'
         return {"type": "PrintStatement", "value": expr}
 
+    def parse_expression(self):
+        return self.parse_term()
 
+    def parse_term(self):
+        node = self.parse_factor()
+        while self.current_token and self.current_token[0] == "OPERATOR" and self.current_token[1] in ('+', '-'):
+            op = self.current_token[1]
+            self.consume("OPERATOR")
+            right = self.parse_factor()
+            node = {"type": "BinaryExpression", "operator": op, "left": node, "right": right}
+        return node
+
+    def parse_factor(self):
+        if self.current_token[0] == 'NUMBER':
+            return self.parse_number()
+        elif self.current_token[0] == 'IDENTIFIER':
+            return self.parse_identifier()
+        else:
+            self.error("Invalid factor")
+
+    def parse_number(self):
+        num = self.current_token[1]
+        self.consume("NUMBER")
+        return {"type": "Number", "value": num}
+
+    def parse_identifier(self):
+        var_name = self.current_token[1]
+        self.consume("IDENTIFIER")
+        return {"type": "Identifier", "name": var_name}
+
+# Main execution
 if __name__ == "__main__":
-    # Read tokens from the symbol table text file
-    tokens = read_tokens_from_symbol_table("symbol_table.txt")
-    print(f"Tokens read from symbol_table.txt: {tokens}")  # Debug print
+    tokens = read_tokens_from_json("tokens.json")
+    print(f"Tokens read from tokens.json: {tokens}")
 
-    # Create parser object
     parser = Parser(tokens)
-
-    # Parse the tokens into an AST
     ast = parser.parse()
 
-    # Display the Abstract Syntax Tree (AST)
     print("Abstract Syntax Tree (AST):")
-    print(ast)
+    print(json.dumps(ast, indent=2))  # Pretty print the AST
